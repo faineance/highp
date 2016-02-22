f767853f7ea0f350cec8927828b7f6c051605395
faineance <faineance@users.noreply.github.com>
2016-02-22 08:25:35 +0000

    how do I c++

--- highp.h
+++ highp.h
@@ -12,6 +12,7 @@ public:
     INLINE vec4() {
         m = _mm_setzero_ps();
     };
+    
     INLINE explicit vec4(float w) {
         m = _mm_set1_ps(w);
     };
@@ -28,42 +29,39 @@ public:
         return vec4((float)x, (float)y, (float)z, (float)w);
     }
 
-    INLINE friend vec4 operator+ (vec4 a, vec4 b) { return _mm_add_ps(a.m, b.m);  }
-    INLINE friend vec4 operator- (vec4 a, vec4 b) { return _mm_sub_ps(a.m, b.m);  }
-    INLINE friend vec4 operator* (vec4 a, vec4 b) { return _mm_mul_ps(a.m, b.m);  }
-    INLINE friend vec4 operator/ (vec4 a, vec4 b) { return _mm_div_ps(a.m, b.m);  }
+    INLINE vec4 operator+ (vec4 b) { return _mm_add_ps(m, b.m);  }
+    INLINE vec4 operator- (vec4 b) { return _mm_sub_ps(m, b.m);  }
+    INLINE vec4 operator* (vec4 b) { return _mm_mul_ps(m, b.m);  }
+    INLINE vec4 operator/ (vec4 b) { return _mm_div_ps(m, b.m);  }
+    INLINE vec4 operator* (float b) { return _mm_mul_ps(m, _mm_set1_ps(b)); }
+    INLINE vec4 operator/ (float b) { return _mm_div_ps(m, _mm_set1_ps(b)); }
 
-    INLINE friend vec4 operator* (vec4 a, float b) { return _mm_mul_ps(a.m, _mm_set1_ps(b)); }
-    INLINE friend vec4 operator/ (vec4 a, float b) { return _mm_div_ps(a.m, _mm_set1_ps(b)); }
-    INLINE friend vec4 operator* (float a, vec4 b) { return _mm_mul_ps(_mm_set1_ps(a), b.m); }
-    INLINE friend vec4 operator/ (float a, vec4 b) { return _mm_div_ps(_mm_set1_ps(a), b.m); }
-
-    INLINE friend vec4 operator==(vec4 a, vec4 b) { return _mm_cmpeq_ps(a.m, b.m); }
-    INLINE friend vec4 operator!=(vec4 a, vec4 b) { return _mm_cmpneq_ps(a.m, b.m); }
-    INLINE friend vec4 operator< (vec4 a, vec4 b) { return _mm_cmplt_ps(a.m, b.m); }
-    INLINE friend vec4 operator> (vec4 a, vec4 b) { return _mm_cmpgt_ps(a.m, b.m); }
-    INLINE friend vec4 operator<=(vec4 a, vec4 b) { return _mm_cmple_ps(a.m, b.m); }
-    INLINE friend vec4 operator>=(vec4 a, vec4 b) { return _mm_cmpge_ps(a.m, b.m); }
-
-
-    INLINE vec4 max(vec4 &v, float f) { return _mm_max_ps(v.m, _mm_set1_ps(f)); }
-
-    INLINE vec4 max(vec4 &a, vec4 &b) { return _mm_max_ps(a.m, b.m); }
-
-    INLINE vec4 min(vec4 &v, float f) { return _mm_min_ps(v.m, _mm_set1_ps(f)); }
-
-    INLINE vec4 min(vec4 &a, vec4 &b) { return _mm_min_ps(a.m, b.m); }
-
-
-    INLINE float sum(vec4 v) { return v.x + v.y + v.z + v.w; }
-
-    INLINE float dot(vec4 a, vec4 b) { return sum(a * b); }
-
-    INLINE float length(vec4 v) { return sqrtf(dot(v, v)); }
-
-    INLINE float length_squared(vec4 v) { return dot(v, v); }
-
-    INLINE vec4 norm(vec4 v) { return v * (1.0f / length(v)); }
+    INLINE vec4 operator==(vec4 b) { return _mm_cmpeq_ps(m, b.m); }
+    INLINE vec4 operator!=(vec4 b) { return _mm_cmpneq_ps(m, b.m); }
+    INLINE vec4 operator< (vec4 b) { return _mm_cmplt_ps(m, b.m); }
+    INLINE vec4 operator> (vec4 b) { return _mm_cmpgt_ps(m, b.m); }
+    INLINE vec4 operator<=(vec4 b) { return _mm_cmple_ps(m, b.m); }
+    INLINE vec4 operator>=(vec4 b) { return _mm_cmpge_ps(m, b.m); }
+//
+//
+//    INLINE vec4 max(vec4 &v, float f) { return _mm_max_ps(v.m, _mm_set1_ps(f)); }
+//
+//    INLINE vec4 max(vec4 &a, vec4 &b) { return _mm_max_ps(a.m, b.m); }
+//
+//    INLINE vec4 min(vec4 &v, float f) { return _mm_min_ps(v.m, _mm_set1_ps(f)); }
+//
+//    INLINE vec4 min(vec4 &a, vec4 &b) { return _mm_min_ps(a.m, b.m); }
+//    INLINE vec4  clamp(vec4 t, vec4 a, vec4 b) { return min(b, max(t, a)); }
+//
+//    INLINE float sum(vec4 v) { return v.x + v.y + v.z + v.w; }
+//
+//    INLINE float dot(vec4 a, vec4 b) { return sum(a * b); }
+//
+//    INLINE float length(vec4 v) { return sqrtf(dot(v, v)); }
+//
+//    INLINE float length_squared(vec4 v) { return dot(v, v); }
+//
+//    INLINE vec4 norm(vec4 v) { return v * (1.0f / length(v)); }
     union {
 
         struct {
