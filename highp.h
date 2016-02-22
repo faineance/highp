9e6e5f84e4dc437f6558f516767c0501637b6729
faineance <faineance@users.noreply.github.com>
2016-02-22 08:28:23 +0000

    yep

--- highp.h
+++ highp.h
@@ -12,7 +12,7 @@ public:
     INLINE vec4() {
         m = _mm_setzero_ps();
     };
-    
+
     INLINE explicit vec4(float w) {
         m = _mm_set1_ps(w);
     };
@@ -42,26 +42,26 @@ public:
     INLINE vec4 operator> (vec4 b) { return _mm_cmpgt_ps(m, b.m); }
     INLINE vec4 operator<=(vec4 b) { return _mm_cmple_ps(m, b.m); }
     INLINE vec4 operator>=(vec4 b) { return _mm_cmpge_ps(m, b.m); }
-//
-//
-//    INLINE vec4 max(vec4 &v, float f) { return _mm_max_ps(v.m, _mm_set1_ps(f)); }
-//
-//    INLINE vec4 max(vec4 &a, vec4 &b) { return _mm_max_ps(a.m, b.m); }
-//
-//    INLINE vec4 min(vec4 &v, float f) { return _mm_min_ps(v.m, _mm_set1_ps(f)); }
-//
-//    INLINE vec4 min(vec4 &a, vec4 &b) { return _mm_min_ps(a.m, b.m); }
-//    INLINE vec4  clamp(vec4 t, vec4 a, vec4 b) { return min(b, max(t, a)); }
-//
-//    INLINE float sum(vec4 v) { return v.x + v.y + v.z + v.w; }
-//
-//    INLINE float dot(vec4 a, vec4 b) { return sum(a * b); }
-//
-//    INLINE float length(vec4 v) { return sqrtf(dot(v, v)); }
-//
-//    INLINE float length_squared(vec4 v) { return dot(v, v); }
-//
-//    INLINE vec4 norm(vec4 v) { return v * (1.0f / length(v)); }
+
+
+    INLINE vec4 max(vec4 &v, float f) { return _mm_max_ps(v.m, _mm_set1_ps(f)); }
+
+    INLINE vec4 max(vec4 &a, vec4 &b) { return _mm_max_ps(a.m, b.m); }
+
+    INLINE vec4 min(vec4 &v, float f) { return _mm_min_ps(v.m, _mm_set1_ps(f)); }
+
+    INLINE vec4 min(vec4 &a, vec4 &b) { return _mm_min_ps(a.m, b.m); }
+    INLINE vec4 clamp(vec4 t, vec4 a, vec4 b) { return min(b, max(t, a)); }
+
+    INLINE float sum(vec4 v) { return v.x + v.y + v.z + v.w; }
+
+    INLINE float dot(vec4 a, vec4 b) { return sum(a * b); }
+
+    INLINE float length(vec4 v) { return sqrtf(dot(v, v)); }
+
+    INLINE float length_squared(vec4 v) { return dot(v, v); }
+
+    INLINE vec4 norm(vec4 v) { return v * (1.0f / length(v)); }
     union {
 
         struct {
